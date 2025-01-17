5c5d70ae2c9258b21239ed761f0c0ca1d584385a
CrusaderJohnny <17ark.leonardis@gmail.com>
2025-01-17 15:24:15 -0700

    Added Nav

--- App.tsx
+++ App.tsx
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaView style={{flex:1}}>
+      <View style={styles.header}>
+        <Text style={styles.headerLight}>
+          OOTD_EVERYDAY
+        </Text>
+        <Text style={styles.headerBold}>
+          Posts
+        </Text>
+      </View>
+      <View style={styles.container}>
+        <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -16,5 +27,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: '#25292e',
+  },
+  header: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  headerBold: {
+    fontSize: 24,
+    fontWeight: 'bold'
+
+  },
+  headerLight: {
+    fontWeight: 'normal',
+    fontSize: 12,    
   },
 });
